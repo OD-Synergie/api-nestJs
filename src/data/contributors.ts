2e7fe8e924b45735b69fa35742e79d16109deffc
odelalande <olivier.delalande-ext@greenway.com>
2024-07-06 18:19:56 +0200

    Add todos

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -2,8 +2,8 @@ export const contributors = [
   {
     firstname: 'Olivier',
     lastname: 'Delalande',
-    linkedin: '',
-    github: '',
+    linkedin: 'https://www.linkedin.com/in/olivierdelalande/',
+    github: 'https://github.com/OD-Synergie',
     contributions: ['PO_1'],
     squads: ['PO_NestJs', 'PO_React'],
   },
--- src/data/todos.ts
+++ src/data/todos.ts
@@ -2,17 +2,57 @@ export const todos = [
   {
     back: [
       {
-        feature: 'add a swagger',
+        feature: 'add a logger',
         ticket_number: 'BACK-1',
         description:
+          'Verify Cors enabling. Explain in README.md cors principles and options',
+      },
+      {
+        feature: 'add a swagger',
+        ticket_number: 'BACK-2',
+        description:
           'Add a swagger. Explain in README.md how to access swagger',
+      },
+      {
+        feature: 'add a logger',
+        ticket_number: 'BACK-3',
+        description:
+          'Add a logger. Explain in README.md how to set logs in the code',
+      },
+      {
+        feature: 'Add a mongo connection',
+        ticket_number: 'BACK-4',
+        description:
+          'Create a mongo connection. Create a todo collection. Create routes to create, read, update and delete todos. Detail in README.md todos structure',
+      },
+      {
+        feature: 'Add a collection for contributors',
+        ticket_number: 'BACK-6',
+        description:
+          'Create a mongo contributors collection. Create routes to create, read, update and delete contributors. Detail in README.md contributors structure',
+      },
+      {
+        feature: 'Add a tool to visualise mogo data',
+        ticket_number: 'BACK-7',
+        description:
+          'Add a tool to visualise mogo data. Detail in README.md how to set it up for people',
       },
     ],
     front: [
       {
-        feature: 'add three routes: home, todos an contributors',
+        feature: 'Make front and back communicate',
         ticket_number: 'FRONT-1',
-        description: 'Add routes and links to navigate',
+        description: 'Use axios to make apps communicate, and disply todos on main page',
+      },
+      {
+        feature: 'add three routes: home, todos an contributors',
+        ticket_number: 'FRONT-2',
+        description: 'Add routes and links to navigate, and list contributors on contributors page',
+      },
+      {
+        feature: 'add shadcn component for cards',
+        ticket_number: 'FRONT-3',
+        description: 'Add a card for each todo and each contributor',
       },
     ],
     ops: [
@@ -21,6 +61,18 @@ export const todos = [
         ticket_number: 'DOCKER-1',
         description:
           'Use last node lts versions for containers, and set up a compose yaml. Detail in README.md how to launch stack easily',
+      },
+      {
+        feature: 'explain how to use rancher, free, when docker desktop is not anymore',
+        ticket_number: 'DOCKER-2',
+        description:
+          'Detail in README.md how to use rancher or another alternative to docker desktop',
+      },
+      {
+        feature: 'Add a container for mongo',
+        ticket_number: 'DOCKER-3',
+        description:
+          'Create a container fo mongo. Mak back container communicate with backen via a network. Explain networks in README',
       },
     ],
     po: [
