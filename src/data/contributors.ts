00ac4348c314d91dd7286499197e8e08e5c6c438
odelalande <olivier.delalande-ext@greenway.com>
2024-08-31 11:36:48 +0200

    Init

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -1,10 +1,11 @@
 export const contributors = [
   {
+    id: 1,
     firstname: 'Olivier',
     lastname: 'Delalande',
     linkedin: 'https://www.linkedin.com/in/olivierdelalande/',
     github: 'https://github.com/OD-Synergie',
-    contributions: ['PO_1'],
-    squads: ['PO_NestJs', 'PO_React'],
+    contributions: ['12'],
+    squads: ['PO_NestJs', 'PO_React', 'PM_Open_Backlog'],
   },
 ];
